aa6521ce7a3ee143dccd392064c643ddccb7aa50
KentoRojas <matiroga123@gmail.com>
2024-09-16 18:40:14 -0300

    Saludos

---
+++ JAVAspring/Condicionales.java
@@ -0,0 +1,9 @@
+public class Condicionales {
+    public static void main(String[] args) {
+        double calificacion;
+        calificacion = 9.3;
+        if (calificacion >= 8.0) {
+            System.out.print("Aprobaste");
+        }
+    }
+}
