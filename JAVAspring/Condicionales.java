3f53de749c9374a946ccab9edb3872a6099564e1
Matias Rojas <matiroga123@gmail.com>
2024-09-16 18:52:33 -0300

    Delete JAVAspring directory

--- JAVAspring/Condicionales.java
+++
@@ -1,9 +0,0 @@
-public class Condicionales {
-    public static void main(String[] args) {
-        double calificacion;
-        calificacion = 9.3;
-        if (calificacion >= 8.0) {
-            System.out.print("Aprobaste");
-        }
-    }
-}
