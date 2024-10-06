829fc8d18cf6c7e6f37c0b888e5f1b651342f0e3
KentoRojas <matiroga123@gmail.com>
2024-10-06 06:09:00 -0300

    Saludos

---
+++ JAVAspring/Variables2.java
@@ -0,0 +1,15 @@
+public class Variables2 {
+
+    public static void main(String[] args) {
+        Integer edad = 20;
+        edad = 15;
+        System.out.println(edad);
+          
+        if (edad > 18) {
+       System.out.println("Eres mayor de edad.");
+        }
+        else{
+        System.out.println("Eres menor de edad.");
+         }
+    }
+}
