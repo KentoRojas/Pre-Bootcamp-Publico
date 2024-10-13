ae4a1a8e0167ad4f87405c3ce35af892532671ee
KentoRojas <matiroga123@gmail.com>
2024-10-13 09:07:50 -0300

    error

--- Click prac/script..js
+++
@@ -1,9 +0,0 @@
-document.getElementById("colorButton").addEventListener("click", function() {
-    cambiarColor();
-});
-
-function cambiarColor() {
-    var colores = ["#FF6347", "#66CDAA", "#9370DB", "#FFD700", "#4682B4", "#FFA07A"];
-    var colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
-    document.body.style.backgroundColor = colorAleatorio;
-}
