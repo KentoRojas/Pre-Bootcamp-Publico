0f71ec8cde81f34288c83b3941894dd5c49e4294
KentoRojas <matiroga123@gmail.com>
2024-10-28 09:40:01 -0300

    core

---
+++ CancionesDelete/Canciones/src/main/java/com/matiasrojas/CancionesApplication.java
@@ -0,0 +1,13 @@
+package com.matiasrojas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CancionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CancionesApplication.class, args);
+	}
+
+}
